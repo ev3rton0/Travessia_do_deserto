4f32092380b7abb834bf966329925bf5ce2689f4
everton <evertonlohan32@gmail.com>
2024-09-10 10:59:02 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+print("TRAVESSIA DO DESERTO\n")
+print("A = -> Consome uma unidade de água.\n    -> Reduz a sede pra zero.\n")
+print("B = -> Avança uma pequena distância.\n    -> Aumenta a sede e o cansaço do camelo.\n    -> Os nativos também avançam.\n")
+print("C = -> Avançar uma longa distância.\n    -> Aumenta significativamente a sede e o cansaço do camelo.\n    -> Os nativos também avançam.\n")
+print("D = -> Parar para descansar.\n    -> Reduz o cansaço do camelo a zero\n    -> Os nativos se aproximam.\n")
+print("E = -> Visualizar o status atual.\n    -> Verificar a distância percorrida.\n    -> Verificar sede.\n    -> Verificar Cansaço do camelo.\n    -> Verificar Distância dos nativos.\n    -> Verificar a quantidade de água restante.\n")
+print("F = -> Desistir da travessia e o jogo termina.\n")
+
+quantidade_agua = 50
+#vida_camelo =
+#avanço_nativos =
+choice_user = input("Digite a ação desejada (A,B,C,D,E,F): ").upper()
+rodadas = 20 
+fim_do_jogo=False
+while not fim_do_jogo:
+    
+
+#while rodadas > 0:
+#    if choice_user=='A':
+
